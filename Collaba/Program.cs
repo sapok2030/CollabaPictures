50ea39488214dd3c47f55792bb3a92d25022be64
sapok2030 <sapok2017@yandex.ru>
2024-02-08 18:38:45 +0300

    f

--- Collaba/Program.cs
+++ Collaba/Program.cs
@@ -10,6 +10,40 @@ namespace Collaba
     {
         static void Main(string[] args)
         {
-        }
-    }
+    Console.Write("Введите значениегла: ");
+string input = Console.ReadLine();
+if (int.TryParse(input, int degree))
+{
+    var trigValue1 = GetTrigValue("sin", 45);
+    var trigValue2 = GetTrigValue(input, degree);
+
+    Calc(trigValue1, trigValue2);
+}
+else
+{
+    Console.WriteLine("Неверный ввод. Пожалуйста, введите целое число.");
+}
+
+// Add more functionalities
+Console.Write("Введите второе значение (для синуса, косинуса, тангенса или котангенса): ");
+string trigFuncInput = Console.ReadLine();
+
+if (Enum.TryParse(trigFuncInput, true, out TrigFunction trigFunc))
+{
+    double trigValue3 = GetTrigValue(trigFuncInput, 0);
+    Calc(trigValue2, trigValue3);
+}
+else
+{
+    Console.WriteLine("Неверный ввод. Пожалуйста, введите синус, косинус, тангенс или котангенс.");
+}
+}
+
+// Add an enumeration for trig functions
+public enum TrigFunction
+{
+    Sin,
+    Cos,
+    Tan,
+    Cot
 }
